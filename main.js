8f3cd2457de621c6759bc5201ed875826213a042
Glitch (hello-webpage) <none>
2020-01-13 17:05:55 +0000

    🏜👨‍❤️‍👨 Checkpoint ./main.css:111157/468 ./main.js:111157/168

--- main.js
+++ main.js
@@ -53,11 +53,14 @@ function togglePassword() {
   if (passwordInput.type === 'password') {
     passwordInput.type = 'text';
     togglePasswordButton.textContent = 'Hide password';
-    togglePasswordButton.setAttribute('aria-label', 'Hide password.');
+    togglePasswordButton.setAttribute('aria-label', 
+       'Hide password.');
   } else {
     passwordInput.type = 'password';
     togglePasswordButton.textContent = 'Show password';
-    togglePasswordButton.setAttribute('aria-label', 'Show password as plain text. Warning: this will display your password on the screen.');
+    togglePasswordButton.setAttribute('aria-label', 
+        'Show password as plain text. ' +
+        'Warning: this will display your password on the screen.');
   }
   return false;
 }
