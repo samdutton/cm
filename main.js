d4679c77b9a31f5063fe2d6270bf27cb417407c2
Glitch (hello-webpage) <none>
2020-01-16 10:53:32 +0000

    🐂🌝 Checkpoint ./main.js:111157/1863

--- main.js
+++ main.js
@@ -1,73 +1,7 @@
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-
 const createAccountButton = document.getElementById('create-account');
 createAccountButton.onclick = () => {location.href='create-account';};
 
 const togglePasswordButton = document.getElementById('toggle-password');
 togglePasswordButton.onclick = togglePassword;
-
-
-
-// Register a user:
-// • Get data from form. 
-// • Upload credential data to server.
-// • Store credentials on client.
-async function register() {
-  const credential = new window.PasswordCredential({
-    id:  emailInput.value,
-    password: passwordInput.value,
-  })
-  
-  // A production app would sanitize and validate the credential data.
-
-
-  registerUserOnServer(credential);
-  await navigator.credentials.store(credential);
-  console.log(`Stored credential data for ${credential.name}`);
-}
-
-async function login() {
-  const credential = await navigator.credentials.get({password: true})
-  if (!credential) {
-    console.log('No credentials stored.');
-  }
-  if (isValid(credential)) {
-    console.log(`You are now logged in as ${credential.name}.`)
-  } else {
-    console.log(`Invalid credentials.`)
-  } 
-}
-
-function logout() {
-  console.log('You are now logged out.');
-}
-
-// A production app would check on your server if the credentials are valid.
-function isValid(credential) {
-  return true;
-}
-
-// Upload credential data to your server to register a new user.
-function registerUserOnServer(credential) {
-  // TODO: POST request to your server.
-  console.log(`Registered user ${credential.name} on server`);
-}
-
-function togglePassword() {  
-  if (passwordInput.type === 'password') {
-    passwordInput.type = 'text';
-    togglePasswordButton.textContent = 'Hide password';
-    togglePasswordButton.setAttribute('aria-label', 
-       'Hide password.');
-  } else {
-    passwordInput.type = 'password';
-    togglePasswordButton.textContent = 'Show password';
-    togglePasswordButton.setAttribute('aria-label', 
-        'Show password as plain text. ' +
-        'Warning: this will display your password on the screen.');
-  }
-  return false;
-}
 
 
