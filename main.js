21f6562cf6fb1321652d6a5b2f0e7b5870b87914
Glitch (hello-webpage) <none>
2020-01-20 11:52:48 +0000

    🖼🍣 Checkpoint ./create-account/index.html:111157/21 ./sign-in/main.js:111157/1268 ./sign-in/index.html:111157/26 ./main.js:111157/263

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
-const createAccountButton = document.getElementById('create-account');
-createAccountButton.onclick = () => {location.href='create-account';};
-
-const togglePasswordButton = document.getElementById('toggle-password');
-togglePasswordButton.onclick = togglePassword;
 
 
---
+++ sign-in/main.js
@@ -0,0 +1,28 @@
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+
+const createAccountButton = document.getElementById('create-account');
+createAccountButton.onclick = createAccount;
+
+const togglePasswordButton = document.getElementById('toggle-password');
+togglePasswordButton.onclick = togglePassword;
+
+function createAccount() {
+  location.href='/create-account';
+}
+
+function togglePassword() {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    togglePasswordButton.textContent = 'Hide password';
+    togglePasswordButton.setAttribute('aria-label',
+       'Hide password.');
+  } else {
+    passwordInput.type = 'password';
+    togglePasswordButton.textContent = 'Show password';
+    togglePasswordButton.setAttribute('aria-label',
+        'Show password as plain text. ' +
+        'Warning: this will display your password on the screen.');
+  }
+}
+  
