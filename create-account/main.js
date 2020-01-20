9b96da12c74229678df302f3ffb07d1bc54b898a
Glitch (hello-webpage) <none>
2020-01-20 14:07:31 +0000

    👣🙆 Checkpoint ./create-account/main.js:111157/1827

--- create-account/main.js
+++ create-account/main.js
@@ -1,2 +1,38 @@
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+
+const createAccountButton = document.getElementById('sign-in');
+createAccountButton.onclick = createAccount;
+
 const signInButton = document.getElementById('sign-in');
-signInButton.onclick = () => {location.href='/sign-in';};
+signInButton.onclick = signIn;
+
+
+// Create account for user:
+// • Get data from form. 
+// • Upload credential data to server.
+// • Store credentials on client.
+async function createAccount() {
+  // TODO: validate and sanitize form data.
+  const credential = new window.PasswordCredential({
+    id:  emailInput.value,
+    password: passwordInput.value,
+  })
+  
+  await createUserOnServer(credential);
+  await navigator.credentials.store(credential);
+  console.log(`Created account for ${credential.id}`);
+}
+
+
+function signIn() {
+  location.href='/sign-in';
+}
+
+// Make a request to your server to create an account.
+//
+async function createUserOnServer(credential) {
+  // TODO: POST request to your server.
+  return console.log(`Registered user ${credential.name} on server`);
+}
+
